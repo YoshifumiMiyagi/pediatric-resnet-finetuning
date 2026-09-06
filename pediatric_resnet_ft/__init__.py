0ff29e6d919ebf000e9a7673c5bcc55f8363b358
yoshifumimiya <49806292+YoshifumiMiyagi@users.noreply.github.com>
2026-09-07 08:37:20 +0900

    Expose dataset CSV builders

--- pediatric_resnet_ft/__init__.py
+++ pediatric_resnet_ft/__init__.py
@@ -1,11 +1,14 @@
 from .model import build_resnet50, set_finetune_mode, trainable_parameter_names
 from .cv import run_cv
+from .metadata import build_dataset_csv, build_age_group_csvs
 
 __all__ = [
     "build_resnet50",
     "set_finetune_mode",
     "trainable_parameter_names",
     "run_cv",
+    "build_dataset_csv",
+    "build_age_group_csvs",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
